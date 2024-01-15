34f727b07c6939b95cb3062c6b0c3cf94e26d214
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2024-01-15 09:53:02 +0100

    remove rate limiter

--- server/authentication/middlewares/authMiddlewares.js
+++ server/authentication/middlewares/authMiddlewares.js
@@ -1,11 +1,11 @@
-import { rateLimit } from 'express-rate-limit';
+// import { rateLimit } from 'express-rate-limit';
 
-export const authRateLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 60 minutes
-    limit: 30, // Limit each IP to 30 requests per window (here, per hour).
-    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-});
+// export const authRateLimiter = rateLimit({
+//     windowMs: 60 * 60 * 1000, // 60 minutes
+//     limit: 30, // Limit each IP to 30 requests per window (here, per hour).
+//     standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+// });
 
 export function isAuthenticated(req, res, next) {
     if (req.session && req.session.user) {
