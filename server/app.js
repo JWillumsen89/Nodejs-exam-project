7764a5a42ee0f517fd6d3ea53202d122c243fa18
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2024-01-16 10:34:20 +0100

    set secure to true

--- server/app.js
+++ server/app.js
@@ -42,7 +42,7 @@ app.use(
         store: sessionStore,
         saveUninitialized: false,
         // cookie: { secure: false },
-        cookie: { secure: false, httpOnly: true, sameSite: 'None' },
+        cookie: { secure: true, httpOnly: true, sameSite: 'None' },
     })
 );
 
